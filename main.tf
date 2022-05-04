26810f1a9c233a882f2f38539fd4734897043407
drewmullen <drmullen@amazon.com>
2022-05-04 13:43:19 -0400

    align to tf standards

--- main.tf
+++ main.tf
@@ -1,13 +1,3 @@
-terraform {
-  required_version = ">= 1.0.5"
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = ">= 3.49.0"
-    }
-  }
-}
-
 data "aws_region" "current" {}
 
 data "aws_vpc" "selected" {
